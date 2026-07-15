fa55517f4f5fcb7f4b3e34ad3c8f80602d1aaf1c
ayushlohani30-lab <ayushlohani30@gmail.com>
2026-07-15 17:48:43 +0530

    Delete Assignment_01_Temperature_Converter.py

--- Assignment_01_Temperature_Converter.py
+++
@@ -1,9 +0,0 @@
-def cel_to_fah(x):
-    return (x*9/5)+32
-
-temperature_in_celsius=int(input("Enter temperature in celsius: "))
-temperature_in_fahrenheit=cel_to_fah(temperature_in_celsius)
-print("Temperature in celsius:",temperature_in_celsius,"°C")
-print("Temperature in fahrenheit:",temperature_in_fahrenheit,"°F")
-
-    
